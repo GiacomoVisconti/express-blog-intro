37e9cf4eb10a7466059eae1db72d9d4a61c4e410
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-06-27 15:52:41 +0200

    add array to project and click fiture

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'))
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 
@@ -9,5 +11,48 @@ app.listen(port, () => {
 
 app.get('/', (req, res)=>{
     console.log("Server del mio blog");
+    res.send('Ciao')
     
+})
+
+app.get('/api/posts/', (req, res) => {
+    const ricette = [
+        {
+            titolo: 'Ricetta Ciambellone',
+            contenuto: 'Testo ricetta',
+            img: 'http://127.0.0.1:3000/img/ciambellone.jpeg',
+            tags: ['dolci', 'colazione', 'ciambellone'],
+
+        },
+        {
+            titolo: 'Ricetta Cracker Barbabietola',
+            contenuto: 'Testo ricetta',
+            img: 'http://127.0.0.1:3000/img/cracker_barbabietola.jpeg',
+            tags: ['snack', 'merenda', 'cracker'],
+
+        },
+        {
+            titolo: 'Ricetta Pane Fritto Dolce',
+            contenuto: 'Testo ricetta',
+            img: 'http://127.0.0.1:3000/img/pane_fritto_dolce.jpeg',
+            tags: ['dolci', 'colazione', 'pane fritto'],
+
+        },
+        {
+            titolo: 'Ricetta Pasta Barbabietola',
+            contenuto: 'Testo ricetta',
+            img: "http://127.0.0.1:3000/img/pasta_barbabietola.jpeg",
+            tags: ['primi piatti', 'pranzo', 'pasta'],
+
+        },
+        {
+            titolo: 'Ricetta Torta Paesana',
+            contenuto: 'Testo ricetta',
+            img: "http://127.0.0.1:3000/img/torta_paesana.jpeg",
+            tags: ['dolci', 'colazione', 'merenda', 'torta'],
+
+        }
+    ]
+
+    res.json(ricette);
 })
